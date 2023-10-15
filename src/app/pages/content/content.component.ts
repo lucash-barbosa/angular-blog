681de48f25fd4741e79c692532a14bb1231b79da
Lucas Henrique <luhcashenrique@gmail.com>
2023-10-15 20:47:44 +0000

    fixing id

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -11,7 +11,7 @@ export class ContentComponent implements OnInit {
   photoCover:string = ""
   contentTitle:string = ""
   contentDescription:string = ""
-  private id:string | null = "0"
+  private id:number = 0
 
   constructor(
     private route:ActivatedRoute
@@ -19,13 +19,13 @@ export class ContentComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-     this.id = value.get("id")
+      this.id = Number(value.get("id"))
     )
 
     this.setValuesToComponent(this.id)
   }
 
-  setValuesToComponent(id:string | null){
+  setValuesToComponent(id:number | null){
     const result = dataFake.filter(article => article.id == id)[0]
 
     this.contentTitle = result.title
